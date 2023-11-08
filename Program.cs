d8049e45e7fcbbfea29a65b814c8665f3fadce24
GusVonDegurechaff <115042946+GusVonDegurechaff@users.noreply.github.com>
2023-11-08 16:06:59 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите x");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите y");
+double y = Convert.ToDouble(Console.ReadLine());
+if (y >= 0 && x >= 0 && y <= 6 - x|| y <= 0 && x <= 0 && y >= -6 - x)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
